b49ef4e5d7260d24d4cb18fd134bd8d371363ac4
Sterling Engle <sengle@e1z2r5p36.42.us.org>
2019-07-14 23:10:37 -0700

    fixed norm

--- ex00/main.c
+++ ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: sengle <sengle@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 19:18:10 by sengle            #+#    #+#             */
-/*   Updated: 2019/07/14 19:58:07 by sengle           ###   ########.fr       */
+/*   Updated: 2019/07/14 23:03:28 by sengle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int		main(void)
 	int size;
 
 	size = -1;
-
 	while (size <= 10)
 		sastantua(size++);
 }
--- ex00/sastantua.c
+++ ex00/sastantua.c
@@ -6,22 +6,24 @@
 /*   By: sengle <sengle@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 18:25:48 by sengle            #+#    #+#             */
-/*   Updated: 2019/07/14 21:06:25 by sengle           ###   ########.fr       */
+/*   Updated: 2019/07/14 23:09:32 by sengle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- * PYRAMID_ROWS(size) macro returns the number of rows for pyramid size
- * DOOR_WIDTH(size) macro returns the width of the door for pyramid size
- */
-#define PYRAMID_ROWS(size)	((2 * size) + ((size * (size + 1)) / 2))
-#define	DOOR_WIDTH(size)	(size - ((size % 2) ? 0 : 1))
+** PM_ROWS(size) macro returns the number of rows for pyramid size
+** DOOR_WIDTH(size) macro returns the width of the door for pyramid size
+*/
+
+#define PM_ROWS(size)	((2 * size) + ((size * (size + 1)) / 2))
+#define DOOR_WIDTH(size)	(size - ((size % 2) ? 0 : 1))
 
 void		ft_putchar(char c);
 
 /*
- * output n copies of character c to standard output using ft_putchar(c)
- */
+** output n copies of character c to standard output using ft_putchar(c)
+*/
+
 static void	putnchars(int n, char c)
 {
 	while (n-- > 0)
@@ -29,37 +31,38 @@ static void	putnchars(int n, char c)
 }
 
 /*
- *	draw_row - displays one row of Sastantua's pyramid
- *
- *			   Local Variables:
- *
- *			   size:	pyramid size
- *			   row:		output row, 0 is first one
- *			   spaces:	number of spaces to display before the '/' character
- *			   stars:	number of stars to display
- *
- *			   Algorithm:
- *
- *			   1. output spaces ' ' before the first '/'
- *			   2. output the '/'
- *			   3. if row >= a row that contains the door:
- *					A. output the '*' before the door
- *					B. if size >= 5 and row equals the middle of the door rows:
- *						a. output '|' door characters leaving one for '$' handle
- *						b. output '$' door handle followed by '|' door edge
- *					C. else output '|' door characters for the entire DOOR_WIDTH(size)
- *					D. output the rest of the '*' after the door
- *				4. else output stars * 2 + 1 '*' characters across the pyramid row
- *				5. output '\' and a newline.
- */
+**	draw_row - displays one row of Sastantua's pyramid
+**
+**			   Local Variables:
+**
+**			   size:	pyramid size
+**			   row:		output row, 0 is first one
+**			   spaces:	number of spaces to display before the '/' character
+**			   stars:	number of stars to display
+**
+**			   Algorithm:
+**
+**			   1. output spaces ' ' before the first '/'
+**			   2. output the '/'
+**			   3. if row >= a row that contains the door:
+**					A. output the '*' before the door
+**					B. if size >= 5 and row equals the middle of the door rows:
+**						a. output '|' door characters leaving one for '$' handle
+**						b. output '$' door handle followed by '|' door edge
+**					C. else output '|' door chars for entire DOOR_WIDTH(size)
+**					D. output the rest of the '*' after the door
+**				4. else output stars * 2 + 1 '*' characters across pyramid row
+**				5. output '\' and a newline.
+*/
+
 static void	draw_row(int size, int row, int spaces, int stars)
 {
 	putnchars(spaces, ' ');
 	ft_putchar('/');
-	if (row >= PYRAMID_ROWS(size) - DOOR_WIDTH(size))
+	if (row >= PM_ROWS(size) - DOOR_WIDTH(size))
 	{
 		putnchars(stars - (DOOR_WIDTH(size) - 1) / 2, '*');
-		if (size >= 5 && row == PYRAMID_ROWS(size) - DOOR_WIDTH(size) / 2 - 1)
+		if (size >= 5 && row == PM_ROWS(size) - DOOR_WIDTH(size) / 2 - 1)
 		{
 			putnchars(DOOR_WIDTH(size) - 2, '|');
 			ft_putchar('$');
@@ -76,30 +79,33 @@ static void	draw_row(int size, int row, int spaces, int stars)
 }
 
 /*
- *	sastantua - displays Sastantua's pyramid of size levels
- *
- *				Local Variables:
- *
- *				level:		 pyramid level where one is the highest
- *				line:		 counts lines within a pyramid level
- *				row:		 pyramid row, row 0 is the first row, stars[] index
- *				stars_count: counts stars within HALF a row of the pyramid
- *				stars[]:	 array of integers containing number of stars per row
- *
- *				Algorithm:
- *
- *				1. while the pyramid level is less than or equal to its size:
- *					A. for each line in the level, calculate the stars_count for that row
- *				2. while the row stars[] index is less than the pyramid's rows:
- *					A. draw the row using draw_row(int size, int row, int spaces, int stars)
- */
+**	sastantua - displays Sastantua's pyramid of size levels
+**
+**				Local Variables:
+**
+**				level:		 pyramid level where one is the highest
+**				line:		 counts lines within a pyramid level
+**				row:		 pyramid row, row 0 is the first row, stars[] index
+**				stars_count: counts stars within HALF a row of the pyramid
+**				stars[]:	 array of integers containing no. of stars per row
+**
+**				Algorithm:
+**
+**				1. while the pyramid level is less than or equal to its size:
+**					A. for each line in the level,
+**					   calculate the stars_count for that row
+**				2. while the row stars[] index is less than the pyramid's rows:
+**					A. draw the row using
+**					   draw_row(int size, int row, int spaces, int stars)
+*/
+
 void		sastantua(int size)
 {
 	int		level;
 	int		line;
 	int		row;
 	int		stars_count;
-	int		stars[PYRAMID_ROWS(size)];
+	int		stars[PM_ROWS(size)];
 
 	level = 1;
 	row = 0;
@@ -118,6 +124,6 @@ void		sastantua(int size)
 			stars_count += (level / 2) + 1;
 	}
 	row = -1;
-	while (++row < PYRAMID_ROWS(size))
-		draw_row(size, row, stars[PYRAMID_ROWS(size) - 1] - stars[row], stars[row]);
+	while (++row < PM_ROWS(size))
+		draw_row(size, row, stars[PM_ROWS(size) - 1] - stars[row], stars[row]);
 }
